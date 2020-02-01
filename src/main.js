ef9c6b5498b7f873d4380403922310d639595c49
SatapornB <marymary2other@gmail.com>
2020-02-01 17:24:44 +0700

    version01

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "popper.js";
+import "./assets/app.scss";
+import jQuery from "jquery";
+import "./views/style.css"
+window.$ = window.jQuery = jQuery;
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
